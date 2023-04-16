cd3acffc916acac6122149331208b66c21e0ba7c
raymondehizoba <rayehizoba@gmail.com>
2023-04-16 11:47:03 +0000

    latest

--- assets/js/page.js
+++ assets/js/page.js
@@ -89,67 +89,67 @@ const projects = [
   //   ]
   // },
   // IA Learn
-  {
-    title: 'Immunization Academy\'s IA Learn',
-    duration: '2021—2023',
-    tags: [
-      'E-Learning',
-      'Android',
-      'Web',
-    ],
-    description: 'Train your immunization team in 15 minutes per day — with an app. ' +
-      'The IA Learn app enables countries to build the capacity of EPI staff ' +
-      'through short instructional videos and tests, taking just 15 minutes per session. ' +
-      'Unlike traditional training, the IA Learn app increases skill and knowledge retention over time ' +
-      'and reduces disruption to work. It puts knowledge right into people\'s hands. ' +
-      'With a library of hundreds of training videos to choose from, ' +
-      'IA Learn can be customized to target a country\'s performance gaps -- and measurably improve performance.',
-    slides: [
-      'assets/images/ia_learn/s1.webp',
-      'assets/images/ia_learn/s2.webp',
-      'assets/images/ia_learn/s3.png',
-      'assets/images/ia_learn/s5.webp',
-      'assets/images/ia_learn/s4.webp',
-    ],
-    links: [
-      {
-        title: 'Homepage',
-        url: 'https://immunizationacademy.com/en/learn_android_app'
-      },
-      {
-        title: 'Play Store',
-        url: 'https://play.google.com/store/apps/details?id=com.bullcitylearning.ialearn'
-      }
-    ]
-  },
+  // {
+  //   title: 'Immunization Academy\'s IA Learn',
+  //   duration: '2021—2023',
+  //   tags: [
+  //     'E-Learning',
+  //     'Android',
+  //     'Web',
+  //   ],
+  //   description: 'Train your immunization team in 15 minutes per day — with an app. ' +
+  //     'The IA Learn app enables countries to build the capacity of EPI staff ' +
+  //     'through short instructional videos and tests, taking just 15 minutes per session. ' +
+  //     'Unlike traditional training, the IA Learn app increases skill and knowledge retention over time ' +
+  //     'and reduces disruption to work. It puts knowledge right into people\'s hands. ' +
+  //     'With a library of hundreds of training videos to choose from, ' +
+  //     'IA Learn can be customized to target a country\'s performance gaps -- and measurably improve performance.',
+  //   slides: [
+  //     'assets/images/ia_learn/s1.webp',
+  //     'assets/images/ia_learn/s2.webp',
+  //     'assets/images/ia_learn/s3.png',
+  //     'assets/images/ia_learn/s5.webp',
+  //     'assets/images/ia_learn/s4.webp',
+  //   ],
+  //   links: [
+  //     {
+  //       title: 'Homepage',
+  //       url: 'https://immunizationacademy.com/en/learn_android_app'
+  //     },
+  //     {
+  //       title: 'Play Store',
+  //       url: 'https://play.google.com/store/apps/details?id=com.bullcitylearning.ialearn'
+  //     }
+  //   ]
+  // },
   // RazorLabs
-  {
-    title: 'Razorlabs',
-    duration: '2022—2023',
-    tags: [
-      'UI/UX Design',
-      'Web',
-    ],
-    description: 'I was the first design hire and head of design at WalletConnect. Here I got to wear all\n' +
-      '                        hats: I was responsible for the company rebranding and redesign of all web and printed\n' +
-      '                        touch-points. I completely overhauled one of web3s most well known products and worked on\n' +
-      '                        introducing completely new ones. I art directed WC\'s brand identity, helped shape its\n' +
-      '                        vision and narrative.',
-    slides: [
-      'assets/images/razorlabs/IMG_1103.PNG',
-      'assets/images/razorlabs/Screenshot 2023-04-08 at 4.26.05 PM.png',
-      'assets/images/razorlabs/IMG_1104.PNG',
-      'assets/images/razorlabs/IMG_1105.PNG',
-      'assets/images/razorlabs/IMG_1106.PNG',
-      'assets/images/razorlabs/Screenshot 2023-04-08 at 4.26.30 PM.png',
-    ],
-    links: [
-      {
-        title: 'Homepage',
-        url: 'https://razorlabs.tech'
-      }
-    ]
-  }
+  // {
+  //   title: 'Razorlabs',
+  //   duration: '2022—2023',
+  //   tags: [
+  //     'UI/UX Design',
+  //     'Web',
+  //   ],
+  //   description: 'I was the first design hire and head of design at WalletConnect. Here I got to wear all\n' +
+  //     '                        hats: I was responsible for the company rebranding and redesign of all web and printed\n' +
+  //     '                        touch-points. I completely overhauled one of web3s most well known products and worked on\n' +
+  //     '                        introducing completely new ones. I art directed WC\'s brand identity, helped shape its\n' +
+  //     '                        vision and narrative.',
+  //   slides: [
+  //     'assets/images/razorlabs/IMG_1103.PNG',
+  //     'assets/images/razorlabs/Screenshot 2023-04-08 at 4.26.05 PM.png',
+  //     'assets/images/razorlabs/IMG_1104.PNG',
+  //     'assets/images/razorlabs/IMG_1105.PNG',
+  //     'assets/images/razorlabs/IMG_1106.PNG',
+  //     'assets/images/razorlabs/Screenshot 2023-04-08 at 4.26.30 PM.png',
+  //   ],
+  //   links: [
+  //     {
+  //       title: 'Homepage',
+  //       url: 'https://razorlabs.tech'
+  //     }
+  //   ]
+  // }
 ];
 
 document.addEventListener('alpine:init', () => {
@@ -205,8 +205,9 @@ const player = document.querySelector("lottie-player");
   const fixedElement = document.querySelector('.foo');
   let windowHeight = window.innerHeight;
   const fixedElementHeight = fixedElement.offsetHeight;
-  let endPosition = windowHeight - fixedElementHeight;
-  // let endPosition = windowHeight - fixedElementHeight;
+  const lastFooterElement = document.querySelector('footer:last-of-type');
+  const lastFooterElementHeight = lastFooterElement.clientHeight;
+  let endPosition = windowHeight - (lastFooterElementHeight - fixedElementHeight);
 
   window.addEventListener('scroll', function () {
     const currentPosition = window.scrollY;
@@ -219,7 +220,7 @@ const player = document.querySelector("lottie-player");
 
   window.addEventListener('resize', function () {
     windowHeight = window.innerHeight;
-    endPosition = windowHeight - fixedElementHeight;
+    endPosition = windowHeight - (lastFooterElementHeight - fixedElementHeight);
   });
 
   // Create a FontFaceObserver instance for your font
